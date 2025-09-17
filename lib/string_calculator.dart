fe19f3654712bcaa2e1a228c0d2b9b19a7154596
shrijeetX <shrijeet100@gmail.com>
2025-09-17 20:40:49 +0530

    Added support for single and multiple numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,8 +1,16 @@
 class StringCalculator {
 
   static int add(String numbers) {
+
     if (numbers.isEmpty) return 0;
-    throw UnimplementedError();
+
+    final splittedString = numbers.split(',');
+
+    if (splittedString.length == 1) {
+      return int.parse(splittedString.first);
+    }
+
+    return splittedString.map(int.parse).reduce((a, b) => a + b);
   }
 
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,8 +3,20 @@ import 'package:test/test.dart';
 
 void main() {
 
-  test('empty string should return 0', () {
-    expect(StringCalculator.add(''), 0);
+  test('empty string should return 0',(){
+    expect(StringCalculator.add(''),0);
+  });
+
+  test('single number should return the number', (){
+    expect(StringCalculator.add('1'),1);
+  });
+
+  test('two numbers comma separated',(){
+    expect(StringCalculator.add('1,5'), 6);
+  });
+
+  test('multiple numbers', (){
+    expect(StringCalculator.add('1,2,3,4'), 10);
   });
 
 }
